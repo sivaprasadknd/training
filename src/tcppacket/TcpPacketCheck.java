50b217e0aaee0e1f2862103918e1bd680f91d15d
Kiranmayee Katyayani Kolluru <katyayani.kolluru@ltu.se>
2018-11-08 22:44:30 +0530

    add

--- src/tcppacket/TcpPacketCheck.java
+++ src/tcppacket/TcpPacketCheck.java
@@ -5,6 +5,11 @@ public class TcpPacketCheck {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println(add(5,10));
+	}
+	public static int add(int a, int b)
+	{
+		return a+b;
 	}
 
 }
